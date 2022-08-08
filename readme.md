8be4eb2a5567fca4154786dbff025e6b72e14d3c
Jhon Adams <jhon@jhonadams.net>
2022-08-07 21:52:04 -0700

    Basic player

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 ### References
 - [The Bevy Book](https://bevyengine.org/learn/book/introduction/)
+- [Johan Helsing's Extreme Bevy](https://johanhelsing.studio/posts/extreme-bevy) ([Github](https://github.com/johanhelsing/extreme_bevy))
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,69 @@
 use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53)))
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
-        .run();
+        .add_startup_system(setup)
+        .add_startup_system(spawn_player)
+        .add_system(move_player)
+        .run()
+}
+
+fn setup(mut commands: Commands) {
+    // let mut camera_bundle = Camera2dBundle::default();
+    // camera_bundle.projection = OrthographicProjection { scale: };
+    // // camera_bundle.orthographic_projection.scale = 1. / 50.;
+    // commands.spawn_bundle(camera_bundle);
+
+    commands.spawn_bundle(Camera2dBundle {
+        projection: OrthographicProjection {
+            scale: 1.,
+            scaling_mode: ScalingMode::FixedVertical(24.0),
+            ..default()
+        }.into(),
+        ..default()
+    });
 }
 
 #[derive(Component)]
-struct Person;
+struct Player;
 
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Elaina Proctor".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Renzo Hume".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Zayna Nieves".to_string()));
+fn spawn_player(mut commands: Commands) {
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0., 0.47, 1.),
+            custom_size: Some(Vec2::new(1., 1.)),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .insert(Player);
 }
 
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
+fn move_player(keys: Res<Input<KeyCode>>, mut player_query: Query<&mut Transform, With<Player>>) {
+    let mut direction = Vec2::ZERO;
+    if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
+        direction.y += 1.;
     }
-}
+    if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
+        direction.y -= 1.;
+    }
+    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
+        direction.x += 1.;
+    }
+    if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
+        direction.x -= 1.;
+    }
+    if direction == Vec2::ZERO {
+        return;
+    }
 
-pub struct HelloPlugin;
+    let move_speed = 0.04;
+    let move_delta = (direction * move_speed).extend(0.);
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        // the reason we call from_seconds with the true flag is to make the timer repeat itself
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
+    for mut transform in player_query.iter_mut() {
+        transform.translation += move_delta;
     }
 }
